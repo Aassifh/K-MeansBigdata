0e91db80798da170a7d87b3a58421516691fb9d7
Hamza Aassif <haassif@emi.u-bordeaux1.fr>
2016-12-22 18:14:14 +0100

    send

--- src/main/java/KmeansNew.java
+++ src/main/java/KmeansNew.java
@@ -2,14 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-
-
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -21,7 +21,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class KmeansNew {
 
-	public static class KMeansNewMapper extends Mapper<Object, Text, DoubleWritable, PointWritable> {
+	public static class KMeansNewMapper extends Mapper<Object, Text, IntWritable, PointWritable> {
 		//private List<Double> Centroids = new ArrayList<Double>();
 
 		int k = 0;
@@ -59,13 +59,14 @@ public class KmeansNew {
 			//Double points [] = new Double[nbColonnes];
 			double nearest = 0.0;
 			for (int i = 0; i < k; i++) {
-			 System.out.println("measuring distance with >>"+context.getConfiguration().getDouble("Center"+i, 0.0));
-				 System.out.println("with"+
-				 Double.valueOf(splits[nbColonnes]));
+//			 System.out.println("measuring distance with >>"+context.getConfiguration().getDouble("Center"+i, 0.0));
+//				 System.out.println("with"+
+//				 Double.valueOf(splits[nbColonnes]));
 				 // should compute the nearest center of a center !!
-				distance = Double.valueOf(splits[nbColonnes]) - context.getConfiguration().getDouble("Center" + i, 0.0);
+				
+				distance = (new PointWritable(Double.valueOf(splits[nbColonnes]))).distance(new PointWritable(context.getConfiguration().getDouble("Center" + i, 0.0)));
 //				 System.out.println("the distance >>"+distance);
-				if (distance < min  && distance <0.0)  {
+				if (distance < min  )  {
 					min = distance;
 					nearest = context.getConfiguration().getDouble("Center" + i, 0.0);
 					indice=i;
@@ -74,20 +75,20 @@ public class KmeansNew {
 
 			}
 			// indice ==> id of the cluster of which the points belongs to
-			context.write(new DoubleWritable(nearest), new PointWritable(indice,Double.valueOf(splits[nbColonnes])));
+			context.write(new IntWritable(indice), new PointWritable(Double.valueOf(splits[nbColonnes])));
 		}
 
-		public void cleanup(Mapper<Object, Text, DoubleWritable, PointWritable>.Context context)
+		public void cleanup(Mapper<Object, Text, IntWritable, PointWritable>.Context context)
 				throws IOException, InterruptedException {
 
 			super.cleanup(context);
 
 		}
 	}
-	public static class KmeansNewCombiner extends Reducer<DoubleWritable, PointWritable, DoubleWritable, PointWritable> {
+	public static class KmeansNewCombiner extends Reducer<IntWritable, PointWritable, IntWritable, PointWritable> {
 		
-		public void reduce(DoubleWritable key, Iterable<PointWritable> values,Context context) throws IOException, InterruptedException {
-			double sum = 0.0;
+		public void reduce(IntWritable key, Iterable<PointWritable> values,Context context) throws IOException, InterruptedException {
+			Double sum = 0.0;
 			int nbelem = 0;
 			//int k =0;
 			// here we will receive a list of point which their key is the
@@ -100,45 +101,52 @@ public class KmeansNew {
 							nbelem++;
 							
 						}
-						System.out.println("sum >>"+sum);
-						System.out.println("nbeleme >>"+nbelem);
-						context.write(key, new PointWritable(nbelem,sum));
+						
+						if(sum.equals(0)){
+							System.out.println("PAS CONTENT");
+						}
+							
+//						System.out.println("sum >>"+sum);
+//						System.out.println("nbeleme >>"+nbelem);
+						context.write(key, new PointWritable(sum/nbelem));
 		}
 	}
 
 	
-	public static class KMeansNewReducer extends Reducer<DoubleWritable, PointWritable, DoubleWritable, PointWritable> {
+	public static class KMeansNewReducer extends Reducer<IntWritable, PointWritable, DoubleWritable, PointWritable> {
 		int k =0;
-		double sum = 0.0;
-		int nbelem = 0;
-		protected void setup(Reducer<DoubleWritable, PointWritable, DoubleWritable, PointWritable>.Context context)
+		protected void setup(Reducer<IntWritable, PointWritable, DoubleWritable, PointWritable>.Context context)
 				throws IOException, InterruptedException {
 			
 			
 			k = context.getConfiguration().getInt("NbCluster", 10);
 		}
 
-		public void reduce(DoubleWritable key, Iterable<PointWritable> values, Context context)
+		public void reduce(IntWritable key, Iterable<PointWritable> values, Context context)
 				throws IOException, InterruptedException {
+			double sum = 0.0;
+			int nbelem = 0;
 			for (PointWritable pointWritable : values) {
 				
 				sum+=pointWritable.Point;
-				nbelem+=pointWritable.nbcluster;
+				nbelem++;
 				
 				context.write(new DoubleWritable(key.get()), pointWritable);
+			
 			}
+			
 			//context.getConfiguration().setDouble("newCenters"+key, sum/nbelem);
 			
 				// il faut envoyer un center pour chaque clé cad chaque cluster 
-			System.out.println("sum >>"+sum);
-			System.out.println("nbeleme >>"+nbelem);
-			for (int i =0; i<k;i++){
-			if( context.getConfiguration().getDouble("Center" + i, 0.0)==key.get())
-				context.getCounter("newCenters",""+i+"" ).setValue((long)(sum/nbelem));
-			}
+//			System.out.println("sum >>"+sum);
+//			System.out.println("nbeleme >>"+nbelem);
+			//for (int i =0; i<k;i++){
+			//if( context.getConfiguration().getDouble("Center" + i, 0.0)==key.get())
+				context.getCounter("newCenters",""+key+"" ).setValue((long)(sum/nbelem));
+			//}
 		}
 
-		public void cleanup(Reducer<DoubleWritable, PointWritable, DoubleWritable, PointWritable>.Context context)
+		public void cleanup(Reducer<IntWritable, PointWritable, DoubleWritable, PointWritable>.Context context)
 				throws IOException, InterruptedException {
 			
 		
@@ -182,12 +190,17 @@ public class KmeansNew {
 	public static boolean iterationChecking(List<Double> oldCenters, List<Double> newCenters, int NbCluster) {
 		// more to come if we have large dimensions
 		double distance = 0.0;
+		
 		for (int i = 0; i < NbCluster; i++) {
 			// not sure
-			distance += oldCenters.get(i) - newCenters.get(i);
+			distance = Math.abs(oldCenters.get(i) - newCenters.get(i));
+			if (distance > 0.01) {
+				return false;
+				 
+			 }
 		}
 
-		return distance < 0.01;
+		return true;
 
 	}
 
@@ -198,19 +211,22 @@ public class KmeansNew {
 		conf.set("Centroid", args[0]);
 		conf.setInt("NbCluster", Integer.valueOf(args[2]));
 		conf.setInt("NbColonnes", Integer.valueOf(args[3]));
+		List<String> Columns = Arrays.asList(Arrays.copyOfRange(args, 3, args.length));
 		Centroids = setCentroids(conf.getInt("NbCluster", 1), conf.get("Centroid"), conf, 0);
 		
 		
 
 		boolean converged = false;
 		int nbiteration = 0;
-		List<Double> newCenters = new ArrayList<Double>();
+		
 		while (!converged) {
 			String output = args[1] + "_" + nbiteration + "_" + System.nanoTime();
 			System.out.println("Centroids >>" + Centroids);
-			for (int i = 0; i < Centroids.size(); i++) {
+			
+			for (int i = 0; i < conf.getInt("NbCluster", 1); i++) {
 				conf.setDouble("Center" + i, Centroids.get(i));
 			}
+			
 			
 			job = Job.getInstance(conf, "Kmeans_iteration" + nbiteration);
 			job.setNumReduceTasks(1);		
@@ -218,32 +234,53 @@ public class KmeansNew {
 			job.setInputFormatClass(TextInputFormat.class);
 			job.setOutputFormatClass(TextOutputFormat.class);
 			job.setMapperClass(KMeansNewMapper.class);
-			job.setMapOutputKeyClass(DoubleWritable.class);
+			job.setMapOutputKeyClass(IntWritable.class);
 			job.setMapOutputValueClass(PointWritable.class);
 			job.setCombinerClass(KmeansNewCombiner.class);
 			job.setReducerClass(KMeansNewReducer.class);
-			job.setOutputKeyClass(DoubleWritable.class);
+			job.setOutputKeyClass(IntWritable.class);
 			job.setOutputValueClass(PointWritable.class);
 			
 			FileInputFormat.addInputPath(job, new Path(args[0]));
 
 			FileOutputFormat.setOutputPath(job, new Path(output));
 			job.waitForCompletion(true);
-			for(int i =0; i<Centroids.size(); i++)
-			System.out.println(" here is the counter "+Double.longBitsToDouble(job.getCounters().findCounter("newCenters",""+i+"").getValue()));
-			for (int i = 0; i < Centroids.size(); i++) {
+			System.out.println("new centroids after job :");
+			for(Double c : Centroids) {
+				System.out.println(">>"+c);
+			}
+			
+			List<Double> newCenters = new ArrayList<Double>();
+				
+			for (int i = 0; i < conf.getInt("NbCluster", 1); i++) {
+				Double newCenter2 = (double)job.getCounters().findCounter("newCenters",""+i+"").getValue();
+				System.out.println(" here is the counter "+newCenter2);
 				// here the fucking bug the getCounter doesnt work !!
 				
 				//  essayer de récupérer !! le nouveaux centres !!
 				
-				newCenters
-						.add(Double.longBitsToDouble(job.getCounters().findCounter("newCenters",""+i+"").getValue()));
+				newCenters.add(newCenter2);
 				// put new centers in onld centroids list
-				 }
+			}
 
-			System.out.println("what the hell is happening  to the number of clusters>>" + conf.getInt("NbCluster", 1));
+			//System.out.println("what the hell is happening  to the number of clusters>>" + conf.getInt("NbCluster", 1));
+			
+			//System.out.println("size  of  centroids >>"+ Centroids.size());
+			
+			
+			
+			System.out.println("new centroids :");
+			for(Double c : newCenters) {
+				System.out.println(">>"+c);
+			}
 			converged = iterationChecking(Centroids, newCenters, conf.getInt("NbCluster", 1));
-			Centroids = converged ? Centroids : newCenters;
+			System.out.println("SIZE OF NEW CENTROIDS"+newCenters.size());
+			Centroids =  newCenters;
+			System.out.println("new centroids :");
+			for(Double c : Centroids) {
+				System.out.println(">>"+c);
+			}
+			System.out.println("SIZE OF CENTROIDS"+Centroids.size());
 			nbiteration++;
 		}
 
--- src/main/java/PointWritable.java
+++ src/main/java/PointWritable.java
@@ -1,6 +1,7 @@
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.hadoop.io.Writable;
 
@@ -13,40 +14,63 @@ import java.io.IOException;
 import org.apache.hadoop.io.Writable;
 
 public class PointWritable implements Writable {
-	public int nbcluster ; 
+	
 	// multi dimension point
 	 public Double Point ;
-	
+	public List<Double> Points;
 	public PointWritable(){
 		
 	}
 	
-	public PointWritable(int nbcluster, Double point) {
+	public PointWritable(Double point) {
 		
-		this.nbcluster = nbcluster;
+		
+		
 		Point = point;
+	}
+	public PointWritable(List<Double> points){
+		this.Points=points;
 	}
 
 	@Override
 	
 	public void readFields(DataInput in) throws IOException {
 		// TODO Auto-generated method stub
-		nbcluster= in.readInt();
+		;
 		Point= in.readDouble();
+		for (Double double1 : Points) {
+			double1=in.readDouble();
+		}
 		
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
 		// TODO Auto-generated method stub
-		out.writeInt(nbcluster);
-		out.writeDouble(Point);
 		
+		out.writeDouble(Point);
+		for (Double double1 : Points) {
+			out.writeDouble(double1);
+		}
+		
+	}
+	public double distance (PointWritable p){
+		 
+		double dist =0.0;
+		for (int i=0 ; i<this.Points.size() && i < p.Points.size();i++){
+			dist+=Math.sqrt(Math.pow(this.Points.get(i)-p.Points.get(i), 2));
+		}
+		
+	return dist;
 	}
 
 	@Override
 	public String toString() {
-		return nbcluster+">>>"+ Point;
+		String s ="Points >>> \n";
+		for (Double double1 : Points) {
+			s+=double1+" ";
+		}
+		return s ;
 	}
 
 	
